369dad294b751ee9718f215840908eb0a868f9fb
Oski <oskar.bialek.programista@gmail.com>
2017-09-21 21:11:16 +0200

    added blank project files, rename game window

---
+++ Ping Pong.mm
@@ -0,0 +1,18 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1506020630343" ID="ID_1471678398" MODIFIED="1506020657621" TEXT="Ping Pong">
+<node CREATED="1506020698408" ID="ID_824409494" MODIFIED="1506020805325" POSITION="right" TEXT="Paletki">
+<node CREATED="1506020709784" ID="ID_461098764" MODIFIED="1506020737819" TEXT="2 paletki poruszajace sie w gore i w dol"/>
+<node CREATED="1506020739118" ID="ID_136925065" MODIFIED="1506020756666" TEXT="sterowanie paletkami: strzalki gora,dol + w,s"/>
+<node CREATED="1506020807445" ID="ID_200542467" MODIFIED="1506020820258" TEXT="dzwiek odbicia pileczki"/>
+</node>
+<node CREATED="1506020770863" HGAP="-246" ID="ID_1960569081" MODIFIED="1506020981240" POSITION="left" TEXT="Pi&#x142;ka" VSHIFT="-149">
+<edge STYLE="linear"/>
+<node CREATED="1506020778741" HGAP="-277" ID="ID_515355769" MODIFIED="1506020992186" TEXT="ruch pi&#x142;ki na podstawie gry arkanoid" VSHIFT="3"/>
+</node>
+<node CREATED="1506020825109" ID="ID_1051130594" MODIFIED="1506020837723" POSITION="right" TEXT="T&#x142;o/st&#xf3;&#x142;">
+<node CREATED="1506020838630" ID="ID_1071161796" MODIFIED="1506020847082" TEXT="Ciemnozielone"/>
+<node CREATED="1506020847652" ID="ID_1791128754" MODIFIED="1506020860418" TEXT="W pozniejszym etapie dodac teksture na srodku przypominajaca prawdziwy stol"/>
+</node>
+</node>
+</map>
